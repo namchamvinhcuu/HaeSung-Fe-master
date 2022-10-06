8c0253184860a8ca9295c083079fb58307b55647
nga0897 <41340836+nga0897@users.noreply.github.com>
2022-10-06 11:03:02 +0700

    permission

---
+++ src/views/components/standard_db/Configuration/Permission/ModifyPermissionDialog.js
@@ -0,0 +1,168 @@
+import { MuiDialog, MuiResetButton, MuiSubmitButton } from '@controls'
+import { yupResolver } from '@hookform/resolvers/yup'
+import {
+    Autocomplete,
+    Checkbox, FormControlLabel, Grid, Radio, RadioGroup, TextField
+} from '@mui/material'
+import React, { useEffect, useRef, useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import { useIntl } from 'react-intl'
+import * as yup from 'yup'
+
+import { permissionService } from '@services'
+import { ErrorAlert, SuccessAlert } from '@utils'
+
+const ModifyPermissionDialog = (props) => {
+
+    const intl = useIntl();
+
+    const { initModal, isOpen, onClose, setModifyData } = props;
+    console.log(initModal);
+
+    const clearParent = useRef(null);
+
+    const dataModalRef = useRef({ ...initModal });
+    const [dialogState, setDialogState] = useState({
+        ...initModal,
+        isSubmit: false,
+    })
+
+  
+
+    const schema = yup.object().shape({
+      
+        forRoot: yup.bool().required(),
+        permissionName: yup.string().required(),
+
+
+    });
+    const { control, register, formState: { errors }, handleSubmit, clearErrors, reset } = useForm({
+        mode: 'onChange',
+        resolver: yupResolver(schema),
+        defaultValues: {
+            ...initModal,
+
+        },
+    });
+
+
+
+    const handleCloseDialog = () => {
+        reset();
+        clearErrors();
+        setDialogState({
+            ...dialogState,
+        })
+        onClose();
+    }
+
+
+
+    const onSubmit = async (data) => {
+       
+        dataModalRef.current = { ...initModal, ...data };
+        setDialogState({ ...dialogState, isSubmit: true });
+
+        const res = await permissionService.modify(dataModalRef.current);
+        if (res.HttpResponseCode === 200 && res.Data) {
+            SuccessAlert(intl.formatMessage({ id: res.ResponseMessage }))
+            setModifyData({ ...res.Data });
+
+            handleCloseDialog();
+        }
+        else {
+            ErrorAlert(intl.formatMessage({ id: res.ResponseMessage }))
+        }
+
+        setDialogState({ ...dialogState, isSubmit: false });
+    };
+
+
+
+    return (
+        <MuiDialog
+            maxWidth='sm'
+            title={intl.formatMessage({ id: 'general.modify' })}
+            isOpen={isOpen}
+            disabledCloseBtn={dialogState.isSubmit}
+            disable_animate={300}
+            onClose={handleCloseDialog}
+        >
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Grid container rowSpacing={2.5} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                   
+                    <Grid item xs={12}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={6}>
+                                <TextField
+                                    autoFocus
+                                    fullWidth
+                                    disabled
+                                    size='small'
+                                    label={intl.formatMessage({ id: 'general.name' })}
+                                    {...register('commonDetailName', {
+                                        // onChange: (e) => handleInputChange(e)
+                                    })}
+                                    error={!!errors?.permissionName}
+                                    helperText={errors?.permissionName ? errors.permissionName.message : null}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <TextField
+                                    autoFocus
+                                    fullWidth
+                                    disabled
+                                    size='small'
+                                    label={intl.formatMessage({ id: 'general.name' })}
+                                    {...register('permissionName', {
+                                        // onChange: (e) => handleInputChange(e)
+                                    })}
+                                    error={!!errors?.permissionName}
+                                    helperText={errors?.permissionName ? errors.permissionName.message : null}
+                                />
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Grid container spacing={2}>
+                          
+                            <Grid item xs={6}>
+                                <FormControlLabel
+                                    control={
+                                        <Controller
+                                            name='forRoot'
+                                            control={control}
+                                            render={({ field: props }) => (
+                                                <Checkbox
+                                                    {...props}
+                                                    checked={props.value}
+                                                    onChange={(e) => props.onChange(e.target.checked)}
+                                                />
+                                            )}
+                                        />
+                                    }
+                                    label='For Root'
+                                />
+                            </Grid>
+                        </Grid>
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <Grid
+                            container
+                            direction="row-reverse"
+                        >
+                            <MuiSubmitButton
+                                text="save"
+                                loading={dialogState.isSubmit}
+                            />
+                          
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </form>
+        </MuiDialog>
+    )
+}
+
+export default ModifyPermissionDialog
--- src/views/components/standard_db/Configuration/Permission/Permission.js
+++ src/views/components/standard_db/Configuration/Permission/Permission.js
@@ -1,7 +1,223 @@
-import React from 'react'
+import DeleteIcon from '@mui/icons-material/Delete'
+import EditIcon from '@mui/icons-material/Edit'
+import Grid from '@mui/material/Grid'
+import IconButton from '@mui/material/IconButton'
+import { createTheme, ThemeProvider } from "@mui/material"
+import React, { useEffect, useRef, useState } from 'react'
+import { useIntl } from 'react-intl'
+import { MuiButton, MuiDataGrid } from '@controls'
+import { permissionService } from '@services'
+import _ from 'lodash'
+import ModifyPermissionDialog from './ModifyPermissionDialog'
 
-export default function Permission() {
+const myTheme = createTheme({
+    components: {
+        //@ts-ignore - this isn't in the TS because DataGird is not exported from `@mui/material`
+        MuiDataGrid: {
+            styleOverrides: {
+                row: {
+                    "&.Mui-created": {
+                        backgroundColor: "#A0DB8E",
+                        //   "&:hover": {
+                        //     backgroundColor: "#98958F"
+                        //   }
+                    }
+                }
+            }
+        }
+    }
+});
+
+const Permission = () => {
+    const intl = useIntl();
+    let isRendered = useRef(false);
+    const initMenuModel = {
+        permissionId: 0
+        , permissionName: ''
+        , commonDetailId: ''
+        , commonDetailName: ''
+        , forRoot: ''
+    }
+
+    const menuGridRef = useRef();
+
+    const [menuState, setMenuState] = useState({
+        isLoading: false,
+        data: [],
+        totalRow: 0,
+        page: 1,
+        pageSize: 50,
+    });
+
+    const [isOpenModifyDialog, setIsOpenModifyDialog] = useState(false)
+
+    const [selectedRow, setSelectedRow] = useState({
+        ...initMenuModel
+    })
+
+    const [newData, setNewData] = useState({ ...initMenuModel })
+
+ 
+
+    const toggleModifyMenuDialog = () => {
+        setIsOpenModifyDialog(!isOpenModifyDialog);
+    }
+
+    const handleRowSelection = (arrIds) => {
+
+        const rowSelected = menuState.data.filter(function (item) {
+            return item.permissionId === arrIds[0]
+        });
+        if (rowSelected && rowSelected.length > 0) {
+            setSelectedRow({ ...rowSelected[0] });
+        }
+        else {
+            setSelectedRow({ ...initMenuModel });
+        }
+    }
+
+    async function fetchData() {
+        setMenuState({
+            ...menuState
+            , isLoading: true
+
+        });
+        const params = {
+            page: menuState.page,
+            pageSize: menuState.pageSize
+        }
+       const res = await permissionService.getPermissionList(params);
+      
+        setMenuState({
+            ...menuState
+            , data: [...res.Data]
+            , totalRow: res.TotalRow
+            , isLoading: false
+        });
+    }
+
+    useEffect(() => {
+       
+        fetchData();
+    }, [menuState.page, menuState.pageSize]);
+
+    useEffect(() => {
+
+        const data = [newData, ...menuState.data]
+        data.pop();
+        setMenuState({
+            ...menuState
+            , data: [...data]
+            , totalRow: menuState.totalRow + 1
+        });
+    }, [newData]);
+
+    useEffect(() => {
+        let newArr = [];
+        if (!_.isEqual(selectedRow, initMenuModel)) {
+            newArr = [...menuState.data]
+            const index = _.findIndex(newArr, function (o) { return o.permissionId == selectedRow.permissionId; });
+            if (index !== -1) {
+                newArr[index] = selectedRow
+            }
+        }
+
+        setMenuState({
+            ...menuState
+            , data: [...newArr]
+        });
+    }, [selectedRow]);
+
+
+
+    const columns = [
+        { field: 'permissionId', headerName: '', flex: 0.01, hide: true},
+        { field: 'id', headerName: '', flex: 0.01,
+            filterable: false,
+        renderCell: (index) => index.api.getRowIndex(index.row.permissionId) + 1,
+        },
+        {
+            field: "action",
+            headerName: "",
+            flex: 0.01,
+            // headerAlign: 'center',
+            disableClickEventBubbling: true,
+            sortable: false,
+            disableColumnMenu: true,
+            renderCell: (params) => {
+                return (
+                    <Grid container spacing={1} alignItems="center" justifyContent="center">
+                        <Grid item xs={12}>
+                            <IconButton
+                                aria-label="edit"
+                                color="warning"
+                                size="small"
+                                sx={[{ '&:hover': { border: '1px solid orange', }, }]}
+                                onClick={toggleModifyMenuDialog}
+                            >
+                                <EditIcon fontSize="inherit" />
+                            </IconButton>
+                        </Grid>
+                    </Grid>
+                );
+            },
+        },
+      
+        
+        { field: 'commonDetailName', headerName: 'Common Detail Name', flex: 0.3, },
+        { field: 'permissionName', headerName: 'Permission Name', flex: 0.3, },
+
+        { field: 'forRoot', headerName: 'forRoot', flex: 0.3, },
+        { field: 'isActived', headerName: 'isActived', flex: 0.3, },
+    ];
+
     return (
-        <div>Permission</div>
+        <React.Fragment>
+            <ThemeProvider theme={myTheme}>
+                <MuiDataGrid
+          
+                    showLoading={menuState.isLoading}
+                    isPagingServer={true}
+                    headerHeight={45}
+                    // rowHeight={30}
+                    columns={columns}
+                    rows={menuState.data}
+                    page={menuState.page - 1}
+                    pageSize={menuState.pageSize}
+                    rowCount={menuState.totalRow}
+                   
+                
+                    rowsPerPageOptions={[50, 100, 200]}
+
+                    onPageChange={(newPage) => {
+                        setMenuState({ ...menuState, page: newPage + 1 });
+                    }}
+                    onPageSizeChange={(newPageSize) => {
+                        setMenuState({ ...menuState, pageSize: newPageSize, page: 1 });
+                    }}
+                    getRowId={(rows) => rows.permissionId}
+                    onSelectionModelChange={(newSelectedRowId) => {
+                        handleRowSelection(newSelectedRowId)
+                    }}
+                  
+                    getRowClassName={(params) => {
+                        if (_.isEqual(params.row, newData)) {
+                            return `Mui-created`
+                        }
+                    }}
+                // 
+                />
+          
+                {isOpenModifyDialog && <ModifyPermissionDialog
+                    initModal={selectedRow}
+                    setModifyData={setSelectedRow}
+                    isOpen={isOpenModifyDialog}
+                    onClose={toggleModifyMenuDialog}
+                />}
+            </ThemeProvider>
+        </React.Fragment>
+
     )
 }
+
+export default Permission
