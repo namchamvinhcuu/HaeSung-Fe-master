a9737a942876d342eb50f56c2ec838a2a6c40a04
namchamvinhcuu <nguyenthanhnam1004@gmail.com>
2022-10-13 11:10:14 +0700

    cm

--- src/models/index.js
+++ src/models/index.js
@@ -2,10 +2,12 @@ import SupplierDto from "./SupplierDto";
 import ProductDto from "./ProductDto";
 import MenuDto from "./MenuDto";
 import StaffDto from "./StaffDto";
+import LineDto from './LineDto'
 
 export {
     SupplierDto,
     ProductDto,
     MenuDto,
     StaffDto,
+    LineDto,
 }
