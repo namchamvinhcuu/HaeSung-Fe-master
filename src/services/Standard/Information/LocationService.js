6de534eecfc11a0c5f1886803364e662bee5a1c5
diemmy39 <tothidiemmy39@gmail.com>
2022-10-19 11:22:58 +0700

    location

--- src/services/Standard/Information/LocationService.js
+++ src/services/Standard/Information/LocationService.js
@@ -21,7 +21,37 @@ const GetArea = async () => {
   }
 }
 
+const createLocation = async (params) => {
+  try {
+    return await axios.post(`${apiName}/create-location`, params);
+  }
+  catch (error) {
+    console.log(`ERROR: ${error}`);
+  }
+}
+
+const modifyLocation = async (params) => {
+  try {
+    return await axios.put(`${apiName}/modify-location`, params);
+  }
+  catch (error) {
+    console.log(`ERROR: ${error}`);
+  }
+}
+
+const deleteLocation = async (params) => {
+  try {
+    return await axios.delete(`${apiName}/delete-location`, { data: params });
+  }
+  catch (error) {
+    console.log(`ERROR: ${error}`);
+  }
+}
+
 export {
   getLocationList,
-  GetArea
+  GetArea,
+  createLocation,
+  modifyLocation,
+  deleteLocation
 }
