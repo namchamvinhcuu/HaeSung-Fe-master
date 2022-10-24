6a80fca9850405d33bdc864d0b44717ba5901ccb
namchamvinhcuu <nguyenthanhnam1004@gmail.com>
2022-10-24 13:08:17 +0700

    cm

--- src/models/PODetailDto.js
+++ src/models/PODetailDto.js
@@ -9,6 +9,14 @@ const PODetailDto = {
     , RemainQty: 0
     , DeliveryDate: moment.utc()
     , DueDate: moment.utc()
+    , isActived: true
+    , createdDate: moment.utc()
+    , createdBy: 0
+    , createdName: 0
+    , modifiedDate: moment.utc()
+    , modifiedBy: 0
+    , modifiedName: 0
+    , row_version: null
 }
 
 export default PODetailDto;
--- src/models/PurchaseOrderDto.js
+++ src/models/PurchaseOrderDto.js
@@ -8,6 +8,14 @@ const PurchaseOrderDto = {
     , DeliveryDate: moment.utc()
     , RemainQty: 0
     , DueDate: moment.utc()
+    , isActived: true
+    , createdDate: moment.utc()
+    , createdBy: 0
+    , createdName: 0
+    , modifiedDate: moment.utc()
+    , modifiedBy: 0
+    , modifiedName: 0
+    , row_version: null
 }
 
 export default PurchaseOrderDto;
