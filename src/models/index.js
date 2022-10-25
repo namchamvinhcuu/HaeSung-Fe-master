687063226c709263d3326ef7f13a9b723c48f1f7
namchamvinhcuu <nguyenthanhnam1004@gmail.com>
2022-10-25 08:36:45 +0700

    cm

--- src/models/index.js
+++ src/models/index.js
@@ -2,26 +2,28 @@ import SupplierDto from "./SupplierDto";
 import ProductDto from "./ProductDto";
 import MenuDto from "./MenuDto";
 import StaffDto from "./StaffDto";
-import LineDto from './LineDto';
-import BuyerDto from './BuyerDto';
-import StandardQCDto from './StandardQCDto';
-import QCMasterDto from './QCMasterDto';
-import QCDetailDto from './QCDetailDto';
+import LineDto from "./LineDto";
+import BuyerDto from "./BuyerDto";
+import StandardQCDto from "./StandardQCDto";
+import QCMasterDto from "./QCMasterDto";
+import QCDetailDto from "./QCDetailDto";
 import LocationDto from "./LocationDto";
 import PurchaseOrderDto from "./PurchaseOrderDto";
 import PODetailDto from "./PoDetailDto";
+import ForecastPODto from "./ForecastPODto";
 
 export {
-    SupplierDto,
-    ProductDto,
-    MenuDto,
-    StaffDto,
-    LineDto,
-    BuyerDto,
-    StandardQCDto,
-    QCMasterDto,
-    QCDetailDto,
-    LocationDto,
-    PurchaseOrderDto,
-    PODetailDto,
-}
+  SupplierDto,
+  ProductDto,
+  MenuDto,
+  StaffDto,
+  LineDto,
+  BuyerDto,
+  StandardQCDto,
+  QCMasterDto,
+  QCDetailDto,
+  LocationDto,
+  PurchaseOrderDto,
+  PODetailDto,
+  ForecastPODto,
+};
