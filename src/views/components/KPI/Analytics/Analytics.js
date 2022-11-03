55f851dab0545fbe0f5b6ff8f7e1d5d43cbd6475
dinhvan <dinhvan47bmt@gmail.com>
2022-11-03 09:36:29 +0700

    up

--- src/views/components/KPI/Analytics/Analytics.js
+++ src/views/components/KPI/Analytics/Analytics.js
@@ -83,7 +83,7 @@ const Analytics = (props) => {
         continue;
       if (i > 52)
         break;
-      col.push({ field: "Week" + i, headerName: "Week" + i, width: 100, align: "right" })
+      col.push({ field: "Week" + i, headerName: "Week " + i, width: 100, align: "right" })
     }
     setGridCol([...col]);
 
