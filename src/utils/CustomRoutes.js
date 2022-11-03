4f0eaa795e1143bac609ea2a07fea80a4c2f2227
namchamvinhcuu <nguyenthanhnam1004@gmail.com>
2022-11-03 21:23:49 +0700

    cm

--- src/utils/CustomRoutes.js
+++ src/utils/CustomRoutes.js
@@ -2,20 +2,20 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 
 const CustomRouter = ({ basename, children, history }) => {
-//   const [state, setState] = React.useState({
-//     action: history.action,
-//     location: history.location,
-//   });
- // React.useLayoutEffect(() => history.listen(setState), [history]);
+  //   const [state, setState] = React.useState({
+  //     action: history.action,
+  //     location: history.location,
+  //   });
+  // React.useLayoutEffect(() => history.listen(setState), [history]);
 
   return (
     <Router
-    history={history}
+      history={history}
       basename={basename}
       children={children}
     //   location={state.location}
     //   navigationType={state.action}
-     
+
     />
   );
 };
