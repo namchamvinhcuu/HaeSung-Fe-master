320df43f17cd4d421c4bda8a03516df17b81a81d
namchamvinhcuu <nguyenthanhnam1004@gmail.com>
2022-11-14 11:28:24 +0700

    cm

--- src/views/components/WMS/Material/ShippingOrder/MaterialSO.js
+++ src/views/components/WMS/Material/ShippingOrder/MaterialSO.js
@@ -5,9 +5,111 @@ import { CombineStateToProps, CombineDispatchToProps } from '@plugins/helperJS'
 import { User_Operations } from '@appstate/user'
 import { Store } from '@appstate'
 
+import { CREATE_ACTION, UPDATE_ACTION } from "@constants/ConfigConstants";
+import {
+    MuiAutocomplete,
+    MuiButton,
+    MuiDataGrid,
+    MuiDateField,
+    MuiSearchField
+} from "@controls";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import UndoIcon from "@mui/icons-material/Undo";
+import { FormControlLabel, Switch } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
+
+import { addDays, ErrorAlert, SuccessAlert } from "@utils";
+import _ from "lodash";
+import moment from "moment";
+
+import { materialSOService } from "@services";
+import { MaterialSOMasterDto, MaterialSODetailDto, } from "@models";
+
 const MaterialSO = (props) => {
     return (
-        <h1>MaterialSO</h1>
+        <React.Fragment>
+            <Grid
+                container
+                spacing={2}
+                justifyContent="flex-end"
+                alignItems="flex-end"
+            >
+                <Grid item xs={1.5}>
+                    <MuiButton
+                        text="create"
+                        color="success"
+                        onClick={() => {
+                            toggleDialog(CREATE_ACTION);
+                        }}
+                    />
+                </Grid>
+
+                <Grid item xs>
+                    <MuiSearchField
+                        label="material-so-master.MsoCode"
+                        name="MsoCode"
+                        onClick={fetchData}
+                        onChange={(e) => changeSearchData(e, "MsoCode")}
+                    />
+                </Grid>
+
+                <Grid item>
+                    <MuiDateField
+                        disabled={workOrderState.isLoading}
+                        label={intl.formatMessage({
+                            id: "general.StartSearchingDate",
+                        })}
+                        value={workOrderState.searchData.StartSearchingDate}
+                        onChange={(e) => {
+                            changeSearchData(e, "StartSearchingDate");
+                        }}
+                        variant="standard"
+                    />
+                </Grid>
+
+                <Grid item xs>
+                    <MuiDateField
+                        disabled={workOrderState.isLoading}
+                        label={intl.formatMessage({
+                            id: "general.EndSearchingDate",
+                        })}
+                        value={workOrderState.searchData.EndSearchingDate}
+                        onChange={(e) => {
+                            changeSearchData(e, "EndSearchingDate");
+                        }}
+                        variant="standard"
+                    />
+                </Grid>
+
+                <Grid item xs={2.5}>
+                    <Grid
+                        container
+                        justifyContent="space-around"
+                        alignItems="flex-end"
+                    >
+                        <Grid item>
+                            <MuiButton text="search" color="info" onClick={fetchData} />
+                        </Grid>
+
+                        <Grid item>
+                            <FormControlLabel
+                                sx={{ mb: 0, ml: "1px" }}
+                                control={
+                                    <Switch
+                                        defaultChecked={true}
+                                        color="primary"
+                                        onChange={(e) => handleshowActivedData(e)}
+                                    />
+                                }
+                                label={showActivedData ? "Actived" : "Deleted"}
+                            />
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </React.Fragment>
     )
 }
 
