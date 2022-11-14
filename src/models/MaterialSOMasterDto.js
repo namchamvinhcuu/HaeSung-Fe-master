2beb3c759fd4272c4abfff9d84546189158017e3
namchamvinhcuu <nguyenthanhnam1004@gmail.com>
2022-11-14 10:55:56 +0700

    cm

---
+++ src/models/MaterialSOMasterDto.js
@@ -0,0 +1,22 @@
+const MaterialSOMasterDto = {
+    MsoId: 0,
+    MsoCode: "",
+    MsoStatus: false,
+    Requester: "",
+    DueDate: new Date(),
+    Remark: "",
+
+    StartSearchingDate: new Date(),
+    EndSearchingDate: new Date(),
+
+    isActived: true,
+    createdDate: new Date(),
+    createdBy: 0,
+    createdName: "",
+    modifiedDate: new Date(),
+    modifiedBy: 0,
+    modifiedName: "",
+    row_version: "",
+};
+
+export default MaterialSOMasterDto;
