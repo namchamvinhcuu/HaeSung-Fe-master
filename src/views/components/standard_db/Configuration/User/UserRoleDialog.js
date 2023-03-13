ac36f58ec5aa1f7664a8f3a28be5371b696e12d4
dinhvan19 <dinhvan47bmt@gmail.com>
2023-03-13 09:28:47 +0700

    up

--- src/views/components/standard_db/Configuration/User/UserRoleDialog.js
+++ src/views/components/standard_db/Configuration/User/UserRoleDialog.js
@@ -1,18 +1,18 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { MuiDialog, MuiResetButton, MuiSubmitButton } from '@controls';
+import { MuiDialog, MuiResetButton, MuiSubmitButton, MuiAutocomplete } from '@controls';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Autocomplete, Checkbox, FormControlLabel, Grid, Radio, RadioGroup, TextField } from '@mui/material';
 import { Controller, useForm } from 'react-hook-form';
 import { useIntl } from 'react-intl';
 import * as yup from 'yup';
-import { userService } from '@services';
+import { userService, workOrderService } from '@services';
 import { ErrorAlert, SuccessAlert } from '@utils';
 
 const UserRoleDialog = ({ isOpen, onClose, setNewData, rowData, loadData }) => {
   const intl = useIntl();
   const [roleList, setRoleList] = useState([]);
   const [roleUser, setRoleUser] = useState([]);
-  const [dialogState, setDialogState] = useState({ isSubmit: false });
+  const [dialogState, setDialogState] = useState({ isSubmit: false, isLine: false });
 
   const schema = yup.object().shape({
     //Roles: yup.object().nullable(intl.formatMessage({ id: 'menu.menuName_required' })),
@@ -84,6 +84,15 @@ const UserRoleDialog = ({ isOpen, onClose, setNewData, rowData, loadData }) => {
     getRolesByUser(rowData.userId);
   }, [rowData]);
 
+  useEffect(() => {
+    let roleApp = roleUser.find((x) => x.roleName == 'WODISPLAY');
+    if (roleApp && roleApp != undefined) setDialogState({ ...dialogState, isLine: true });
+    else {
+      setDialogState({ ...dialogState, isLine: false });
+      setValue('LineId', null);
+    }
+  }, [roleUser]);
+
   return (
     <MuiDialog
       maxWidth="sm"
@@ -131,7 +140,22 @@ const UserRoleDialog = ({ isOpen, onClose, setNewData, rowData, loadData }) => {
               }}
             />
           </Grid>
-
+          {dialogState.isLine && (
+            <Grid item xs={12}>
+              <MuiAutocomplete
+                defaultValue={rowData.LineId ? { LineId: rowData.LineId, LineName: rowData.LineName } : null}
+                label={intl.formatMessage({ id: 'work_order.LineName' })}
+                disabled={dialogState.isSubmit}
+                fetchDataFunc={workOrderService.getLineArr}
+                displayValue="LineId"
+                displayLabel="LineName"
+                onChange={(e, value) => {
+                  setValue('LineId', value?.LineId || null);
+                  setValue('LineName', value?.LineName || '');
+                }}
+              />
+            </Grid>
+          )}
           <Grid item xs={12}>
             <Grid container direction="row-reverse">
               <MuiSubmitButton text="save" loading={dialogState.isSubmit} />
