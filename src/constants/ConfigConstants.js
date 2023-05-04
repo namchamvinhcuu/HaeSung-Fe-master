ee5482cdb72574aafa3508b4e6fc87d4cff46ea7
Truong <truong.tran0813@gmail.com>
2023-05-04 16:39:36 +0700

    material stock

--- src/constants/ConfigConstants.js
+++ src/constants/ConfigConstants.js
@@ -1,8 +1,8 @@
 /**
  * Local
  */
-export const API_URL = 'https://localhost:44301/api/';
-export const BASE_URL = 'https://localhost:44301';
+export const API_URL = 'http://localhost:5261/api/';
+export const BASE_URL = 'http://localhost:5261';
 
 /**
  * Autonsi
