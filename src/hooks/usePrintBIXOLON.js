b9316a5e1848d5ca74fb0544bb8facd268ebc835
Truong <truong.tran0813@gmail.com>
2023-05-05 10:30:02 +0700

    print bixolon

--- src/hooks/usePrintBIXOLON.js
+++ src/hooks/usePrintBIXOLON.js
@@ -40,7 +40,7 @@ const usePrintBIXOLON = () => {
     return JSON.stringify(label_data.current);
   };
 
-  const handlePrint = async ({ detail }) => {
+  const handlePrint = async (detail) => {
     setLabelId(issueID.current);
     clearBuffer();
     directDrawText('SM0,0');
@@ -48,9 +48,9 @@ const usePrintBIXOLON = () => {
     directDrawText('SF1');
     directDrawText('SA0');
     directDrawText('TA0');
-    directDrawText('SL400,24,G');
+    directDrawText('SL64,24,G');
     directDrawText('SOT');
-    directDrawText('SW799');
+    directDrawText('SW400');
     directDrawText('CUTn');
     for (let i = 0; i < detail.length; i++) {
       PrintLabel(detail[i]);
@@ -63,6 +63,7 @@ const usePrintBIXOLON = () => {
     const strSubmit = getLabelData();
 
     issueID.current++;
+
     requestPrint('Printer1', strSubmit, (result) => {
       let success = result.split(':')[1];
       if (success == 'success') SuccessAlert('Success');
@@ -70,59 +71,11 @@ const usePrintBIXOLON = () => {
     });
   };
 
-  const PrintLabel = ({
-    MATERIAL_NUMBER,
-    DESCRIPTION_LOC,
-    TOTAL_QTY,
-    BP_NM,
-    ORIGIN_CODE,
-    PLANT_CODE,
-    SL_CD,
-    ITEM_ACCT,
-    LOT_CD,
-    createdDate,
-    MFG_DT,
-  }) => {
-    const len = DESCRIPTION_LOC.length;
-    const halfLen = Math.floor(len / 2);
-    const arrPartName = [DESCRIPTION_LOC.substr(0, halfLen), DESCRIPTION_LOC.substr(halfLen)];
-
-    directDrawText(`V19,38,U,23,23,0,N,N,N,0,L,0,'Part No'`);
-    directDrawText(`B1161,25,1,2,6,46,0,0,'${MATERIAL_NUMBER}'`);
-    directDrawText(`V173,4,U,23,23,0,N,N,N,0,L,0,'${MATERIAL_NUMBER}'`);
-    directDrawText(`V19,89,U,23,23,0,N,N,N,0,L,0,'Part name'`);
-    directDrawText(`V161,80,U,23,23,0,N,N,N,0,L,0,'${arrPartName[0]}'`);
-    directDrawText(`V161,103,U,23,23,0,N,N,N,0,L,0,'${arrPartName[1]}'`);
-    directDrawText(`V19,137,U,23,23,0,N,N,N,0,L,0,'Qty'`);
-    directDrawText(`V161,140,U,23,23,0,N,N,N,0,L,0,'${TOTAL_QTY}'`);
-    directDrawText(`V19,180,U,23,23,0,N,N,N,0,L,0,'Lot Code'`);
-    directDrawText(`V161,180,U,23,23,0,N,N,N,0,L,0,'${LOT_CD}'`);
-    directDrawText(`V73,220,U,23,23,0,N,N,N,0,C,0,'Receiving'`);
-    directDrawText(`V73,243,U,23,23,0,N,N,N,0,C,0,' Date'`);
-    directDrawText(`V161,230,U,23,23,0,N,N,N,0,L,0,'${createdDate}'`);
-    directDrawText(`V19,279,U,23,23,0,N,N,N,0,L,0,'VENDOR'`);
-    directDrawText(`V161,279,U,23,23,0,N,N,N,0,L,0,'${BP_NM}'`);
-    directDrawText(`V19,319,U,23,23,0,N,N,N,0,L,0,'MFG Date'`);
-    directDrawText(`V161,319,U,23,23,0,N,N,N,0,L,0,'${MFG_DT}'`);
-    directDrawText(`V19,359,U,23,23,0,N,N,N,0,L,0,'Original'`);
-    directDrawText(`V161,357,U,23,23,0,N,N,N,0,L,0,'${ORIGIN_CODE}'`);
-    directDrawText(`B2622,230,Q,2,M,5,0,'${LOT_CD}'`);
-    directDrawText(`V588,23,U,23,23,0,N,N,N,0,L,0,'Solumn Vina'`);
-    directDrawText(`V569,57,U,23,23,0,N,N,N,0,L,0,'PLAN'`);
-    directDrawText(`V694,59,U,23,23,0,N,N,N,0,L,0,'${PLANT_CODE}'`);
-    directDrawText(`V569,97,U,23,23,0,N,N,N,0,L,0,'Location'`);
-    directDrawText(`V694,97,U,23,23,0,N,N,N,0,L,0,'${SL_CD}'`);
-    directDrawText(`V634,140,U,23,23,0,N,N,N,0,L,0,'${ITEM_ACCT}'`);
+  const PrintLabel = ({ MaterialCode }) => {
+    directDrawText(`B182,4,1,1,3,35,0,0,'>A${MaterialCode}'`);
+    directDrawText(`V68,40,U,23,23,0,N,N,N,0,L,0,'${MaterialCode}'`);
   };
 
-  useEffect(() => {
-    window.addEventListener('printBIXILON', handlePrint);
-
-    return () => {
-      window.removeEventListener('printBIXILON', handlePrint);
-    };
-  }, []);
-
-  return null;
+  return { printBIXILON: handlePrint };
 };
 export default usePrintBIXOLON;
