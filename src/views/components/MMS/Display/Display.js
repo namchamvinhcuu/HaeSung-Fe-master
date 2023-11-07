3a42880362abda3dfdab912fa5e6fbfce073a1d5
Trần Văn Trường <truong.tran0813@gmail.com>
2023-11-07 15:42:45 +0700

    fix: Display

--- src/views/components/MMS/Display/Display.js
+++ src/views/components/MMS/Display/Display.js
@@ -22,7 +22,7 @@ const Display = (props) => {
   const intl = useIntl();
   const handle = useFullScreenHandle();
   //const [data, setData] = useState({ totalOrderQty: 0, totalActualQty: 0, totalEfficiency: 0, data: [] });
-  const { totalOrderQty, totalActualQty, totalEfficiency, data } = props;
+  const { data } = props;
   const style = {
     grid: {
       textAlign: 'center',
@@ -34,6 +34,35 @@ const Display = (props) => {
       minHeight: '93px',
     },
   };
+  const [displayData, setDisplayData] = useState({
+    totalOrderQty: 0,
+    totalActualQty: 0,
+    totalEfficiency: 0,
+    data: [],
+  });
+  useEffect(() => {
+    let rowData = [];
+    data?.forEach((ele) => {
+      const index = rowData.findIndex(
+        (x) =>
+          x.woProcess == ele.woProcess && parseInt(ele?.lineName?.split('#')[1]) == parseInt(x?.lineName?.split('#')[1])
+      );
+
+      if (index == -1 && parseInt(ele?.lineName?.split('#')[1]) > 0) {
+        let efficiency = Math.round((ele.actualQty / ele.orderQty) * 100);
+        rowData.push({ ...ele, efficiency });
+      }
+    });
+    let totalOrderQty = rowData.length > 0 ? rowData.map((x) => x.orderQty).reduce((a, b) => a + b) : 0;
+    let totalActualQty = rowData.length > 0 ? rowData.map((x) => x.actualQty).reduce((a, b) => a + b) : 0;
+    setDisplayData({
+      data: rowData,
+      totalOrderQty,
+      totalActualQty,
+      totalEfficiency: Math.round((totalActualQty / totalOrderQty) * 100),
+    });
+    console.log(rowData);
+  }, [data]);
 
   return (
     <React.Fragment>
@@ -62,15 +91,17 @@ const Display = (props) => {
             <div style={{ height: '13%', display: 'flex' }}>
               <div style={{ ...style.grid, width: '33.3333%', backgroundColor: '#9370db' }}>
                 <h2 style={{ fontWeight: '600', fontFamily: 'Arial, Helvetica, sans-serif' }}>Total Target</h2>
-                <h1 style={{ fontWeight: '600' }}>{Number(totalOrderQty).toLocaleString()}</h1>
+                <h1 style={{ fontWeight: '600' }}>{Number(displayData.totalOrderQty).toLocaleString()}</h1>
               </div>
               <div style={{ ...style.grid, width: '33.3333%', backgroundColor: '#00c6bb' }}>
                 <h2 style={{ fontWeight: '600' }}>Total Actual</h2>
-                <h1 style={{ fontWeight: '600' }}>{Number(totalActualQty).toLocaleString()}</h1>
+                <h1 style={{ fontWeight: '600' }}>{Number(displayData.totalActualQty).toLocaleString()}</h1>
               </div>
               <div style={{ ...style.grid, width: '33.3333%', backgroundColor: '#e9a424' }}>
                 <h2 style={{ fontWeight: '600' }}>Avg Efficiency</h2>
-                <h1 style={{ fontWeight: '600' }}>{totalEfficiency > 100 ? 100 : Math.round(totalEfficiency)}%</h1>
+                <h1 style={{ fontWeight: '600' }}>
+                  {displayData.totalEfficiency > 100 ? 100 : Math.round(displayData.totalEfficiency)}%
+                </h1>
               </div>
             </div>
             <div style={{ height: '87%', display: 'flex' }}>
@@ -112,9 +143,8 @@ const Display = (props) => {
                 </div>
               </div>
               <div style={{ width: '80%', height: '100%', display: 'flex', overflow: 'auto', whiteSpace: 'nowrap' }}>
-                {data &&
-                  data.map((item, index) => {
-                    let efficiency = Math.round((item.actualQty / item.orderQty) * 100);
+                {displayData &&
+                  displayData.data.map((item, index) => {
                     return (
                       <div style={{ width: '100%', height: '100%' }} key={index}>
                         <div style={{ ...style.grid, height: '14.22%', display: 'grid', border: 'solid 1px #222222' }}>
@@ -136,7 +166,7 @@ const Display = (props) => {
                           <h2 style={{ fontWeight: '600' }}>{Number(item.actualQty).toLocaleString()}</h2>
                         </div>
                         <div style={{ ...style.grid, height: '14.22%', display: 'grid', border: 'solid 1px #222222' }}>
-                          <h2 style={{ fontWeight: '600' }}>{efficiency > 100 ? 100 : efficiency}%</h2>
+                          <h2 style={{ fontWeight: '600' }}>{item.efficiency}%</h2>
                         </div>
                       </div>
                     );
